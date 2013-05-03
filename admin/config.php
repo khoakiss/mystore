3196694ab83cb8d3fb22460665850faae974a9ba
you <you@domain.com>
2013-05-03 20:33:38 +0700

    source code

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://local.store.vn/admin/');
+define('HTTP_CATALOG', 'http://local.store.vn/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://local.store.vn/admin/');
+define('HTTPS_CATALOG', 'http://local.store.vn/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\store/admin/');
+define('DIR_SYSTEM', 'C:\wamp\www\store/system/');
+define('DIR_DATABASE', 'C:\wamp\www\store/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\store/admin/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\store/admin/view/template/');
+define('DIR_CONFIG', 'C:\wamp\www\store/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\store/image/');
+define('DIR_CACHE', 'C:\wamp\www\store/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\store/download/');
+define('DIR_LOGS', 'C:\wamp\www\store/system/logs/');
+define('DIR_CATALOG', 'C:\wamp\www\store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ catalog/language/english/common/header.php
@@ -0,0 +1,13 @@
+<?php
+// Text
+$_['text_home']           = 'Home';
+$_['text_wishlist']       = 'Wish List (%s)';
+$_['text_shopping_cart']  = 'My Cart';
+$_['text_search']         = 'Search';
+$_['text_welcome']        = 'Welcome to our online store!';
+$_['text_logged']         = '<i>Welcome, %s</i>';
+$_['text_account']        = 'My Account';
+$_['text_checkout']       = 'Checkout';
+$_['text_login']		  = '<a href="%s">Login</a>';
+$_['text_logout']		  = '<a href="%s">Logout</a>';
+?>
